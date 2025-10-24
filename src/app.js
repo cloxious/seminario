a8d090ac6f4a513ff0628c7b105fdc2723b83596
lucabotteri169@outlook.com <lucabotteri169@outlook.com>
2025-10-24 00:43:40 -0600

    Actualización de base de datos, app.js y eliminación de config/cuenta.js

--- src/app.js
+++ src/app.js
@@ -1,24 +1,34 @@
-import "dotenv/config";
 import express from "express";
-import { crearCuentaAdmin } from "./config/cuenta.js";
-import { database } from "./config/database.js";
-import usuarioRutas from "./rutas/usuario.ruta.js";
+import morgan from 'morgan';
+import { database } from './config/database.js'; // asegúrate de exportar database con export
 
+import 'dotenv/config';
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/usuarios", usuarioRutas);
+// Función asíncrona principal
+(async () => {
+  try {
+    await database.authenticate();
+    console.log('Conexión exitosa a la base de datos');
+    
+    } catch (err) {
+    console.error('Error al conectar o sincronizar:', err);
+  }
+})();
 
-try {
-  await database();
-  await crearCuentaAdmin();
+// Configuración
+const PORT = process.env.PORT || 3001;
+app.set('port', PORT);
 
-  app.listen(port, () => {
-    console.log(`Servidor iniciado correctamente en el puerto ${port}.`);
-  });
-} catch (error) {
-  console.error("Se produjo un error al iniciar el servidor:", error);
-}
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+
+// Levantar servidor
+app.listen(app.get('port'), () => {
+  console.log('Servidor Funcionando en puerto ' + app.get('port'));
+});
+
